ab5df206a1148670063828d1bfff4ae73a6fa5cc
Neha P Byalal/np_lab/1NT17IS108 <54633548+neha2099@users.noreply.github.com>
2025-11-17 10:53:21 +0530

    home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -103,7 +103,7 @@ const lights = Array.from({ length: 20 }, (_, i) => (
           </Link>
           <Link to="/Croatia" className="category-card christmas-card">
             <img src={Img3} alt="Croatia" />
-            <span>Budapest 🎁</span>
+            <span>Croatia 🎁</span>
           </Link>
         </div>
       </section>
